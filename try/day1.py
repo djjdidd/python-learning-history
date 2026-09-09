eaf26cfc8f1c71e3a484bce43388d0ca88063d25
bbbb <3459616843@qq.com>
2026-09-09 14:28:26 +0800

    new try

--- try/day1.py
+++ try/day1.py
@@ -21,17 +21,34 @@ def width(binary):
 target_width = 0.40
 def objective(width):
     return (width-target_width)**2
-dose_list=np.array([0.8,0.9,1.0,1.1,1.2])
+dose_list=np.array([0.6,0.7,0.8,0.9,1.0,1.1,1.2])
 width_list=[]
 ob_list=[]
-for i in dose_list:
-    binary=printed_pattern(i)
+w1=1
+dose_index=0
+for i,dose in enumerate(dose_list):
+    binary=printed_pattern(dose)
     w=width(binary)
     ob=objective(w)
     width_list.append(w)
     ob_list.append(ob)
     print("dose=",i,"width=",w,"ob=",ob)
+    if objective(w1)<objective(w):
+        w1=w1
+    else:
+        w1=w
+        dose_index=i
+print("best width=",w1)   
+print("best dose=",dose_list[dose_index])
+print("best ob=",objective(w1))
 width_list=np.array(width_list)
 ob_list=np.array(ob_list)
-
-
+plt.figure(figsize=(12,5))
+plt.scatter(dose_list,width_list,label="width")
+plt.plot(dose_list,width_list,label="width")
+plt.xlabel("dose")
+plt.ylabel("width")
+plt.title("width vs dose")
+plt.grid(True)
+plt.legend()
+plt.show()
